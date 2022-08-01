3f0908fb3d6574cd9944b240ccde0b6cb13a5945
dzulix <dzulix@outlook.com>
2022-08-01 09:34:41 +0200

    MTTL-3070: Add Process Information for Change of Name process.

--- src/views/activities-process-view/activities-process-view.test.tsx
+++ src/views/activities-process-view/activities-process-view.test.tsx
@@ -25,7 +25,6 @@ const mockProcess = {
 
 beforeEach(() => {
   get("/api/v1/reference-data", mockEqualityData);
-  get("/api/v1/process/soletojoint/123", mockProcess);
   get("/api/v1/tenures/e67862f3-ccbf-4c51-b8ed-ed1d0420ea19", generateMockTenureV1());
   get("/api/activityhistory", {
     results: [mockStartedProcess],
@@ -35,9 +34,42 @@ beforeEach(() => {
   });
 });
 
-test("renders the activities view", async () => {
+test("renders the activities view processName=soletotjoint", async () => {
+  get("/api/v1/process/soletojoint/123", { ...mockProcess, processName: "soletojoint" });
   const [{ container }] = routeRender(<ActivitiesProcessView entityType="process" />, {
     url: "/activities/process/soletojoint/123",
+    path: "/activities/process/:processName/:id",
+  });
+  await waitForElementToBeRemoved(screen.queryAllByText(/Loading/));
+  await waitFor(() =>
+    expect(screen.getAllByRole("heading")[0]).toHaveTextContent(
+      locale.activities.pageTitle,
+    ),
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("renders the activities view processName=changeofname", async () => {
+  get("/api/v1/process/changeofname/123", {
+    ...mockProcess,
+    processName: "changeofname",
+    previousStates: [
+      {
+        ...mockProcessV1.currentState,
+        state: "NameSubmitted",
+        processData: {
+          formData: {
+            firstName: "First Name",
+            surname: "Surname",
+            title: "Mr",
+            middleName: "Middle Name",
+          },
+        },
+      },
+    ],
+  });
+  const [{ container }] = routeRender(<ActivitiesProcessView entityType="process" />, {
+    url: "/activities/process/changeofname/123",
     path: "/activities/process/:processName/:id",
   });
   await waitForElementToBeRemoved(screen.queryAllByText(/Loading/));
--- src/views/activities-process-view/activities-process-view.tsx
+++ src/views/activities-process-view/activities-process-view.tsx
@@ -1,70 +1,23 @@
 import React from "react";
 import { Link as RouterLink, useParams } from "react-router-dom";
 
+import {
+  ChangeOfNameInformation,
+  SoleToJointInformation,
+} from "./activities-process-information";
+
 import { Button, ErrorSummary } from "@mtfh/common";
 import { useProcess } from "@mtfh/common/lib/api/process/v1";
-import { useTenure } from "@mtfh/common/lib/api/tenure/v1";
-import { Center, Heading, Link, Spinner } from "@mtfh/common/lib/components";
+import { Center, Spinner } from "@mtfh/common/lib/components";
 
 import { ActivityHistoryList } from "@components";
 import { ActivityProcessName, EntityType, locale } from "@services";
 
-const { pageTitle, closeButton } = locale.activities;
+const { closeButton } = locale.activities;
 
-export interface EntityRequestId {
-  targetId: string;
-  incomingTenantId: string | undefined;
-  processName: ActivityProcessName;
-}
-
-const ProcessInformation = ({
-  targetId,
-  processName,
-  incomingTenantId,
-}: EntityRequestId) => {
-  const { data: tenure, error } = useTenure(targetId);
-  if (error) {
-    return (
-      <ErrorSummary
-        id="person-information-view"
-        title={locale.errors.unableToFetchRecord}
-        description={locale.errors.unableToFetchRecordDescription}
-      />
-    );
-  }
-
-  if (!tenure) {
-    return (
-      <Center>
-        <Spinner />
-      </Center>
-    );
-  }
-
-  const tenant = tenure.householdMembers.find((m) => m.isResponsible);
-
-  const incomingTenant = incomingTenantId
-    ? tenure?.householdMembers.find((m) => m.id === incomingTenantId)
-    : undefined;
-
-  return (
-    <>
-      <h1 className="lbh-heading-h1">{pageTitle}</h1>
-      <h2 className="lbh-heading-h2">{locale.process.title[processName]}</h2>
-      <h2 className="lbh-heading-h2">
-        {locale.activities.tenurePaymentRef} {tenure?.paymentReference}
-      </h2>
-      <h2 className="lbh-heading-h2">{tenure?.tenuredAsset.fullAddress}</h2>
-      {tenant && (
-        <Heading variant="h2">
-          <Link as={RouterLink} to={`/person/${tenant.id}`} variant="link">
-            {tenant?.fullName}
-          </Link>
-          {incomingTenant && ` adding ${incomingTenant.fullName}`}
-        </Heading>
-      )}
-    </>
-  );
+const processInformationComponents: Record<string, any> = {
+  soletojoint: SoleToJointInformation,
+  changeofname: ChangeOfNameInformation,
 };
 
 export const ActivitiesProcessView = ({
@@ -100,21 +53,11 @@ export const ActivitiesProcessView = ({
     );
   }
 
-  const { previousStates, currentState } = process;
-  const automatedChecksPassedState = previousStates?.find(
-    (item) => item.state === "AutomatedChecksPassed",
-  );
-  const incomingTenantId =
-    currentState?.processData?.formData?.incomingTenantId ||
-    automatedChecksPassedState?.processData?.formData?.incomingTenantId;
+  const ProcessInformation = processInformationComponents[processName.toLowerCase()];
 
   return (
     <div data-testid="process-activities">
-      <ProcessInformation
-        targetId={process.targetId}
-        processName={processName}
-        incomingTenantId={incomingTenantId}
-      />
+      {ProcessInformation && <ProcessInformation process={process} />}
       <ActivityHistoryList targetId={id} entityType={entityType} />
       <Button as={RouterLink} to={`/processes/${processName}/${id}`} variant="secondary">
         {closeButton}
