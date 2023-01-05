4eca4bc84e6561c17817686694bcd1b6b4145e52
humulla <43747248+humulla@users.noreply.github.com>
2023-01-05 12:27:11 +0000

    unskip one test

--- src/components/activity-history-list/activity-history-list.spec.tsx
+++ src/components/activity-history-list/activity-history-list.spec.tsx
@@ -510,7 +510,7 @@ test("it should display a row for started sole to joint process", async () => {
   expect(container).toMatchSnapshot();
 });
 
-test.skip("it should display activity history for sole to joint process", async () => {
+test("it should display activity history for sole to joint process", async () => {
   get("/api/activityhistory", {
     results: [
       [{ state: "SelectTenants" }, { state: "AutomatedChecksPassed" }],
